741c77fba995748031d52388e78fe0a460ea1094
ngoncalvesrodrigues <nunopedro.goncalvesrodrigues@soprasteria.com>
2021-02-16 04:58:28 +0100

    Initial config

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+app.use(cors());
+
+app.use("/", (req, res) => {
+  res.json({ success: true });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  console.log(`Server listening on port ${process.env.PORT}!`)
+);
